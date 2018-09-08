8d2cd2f634ce5e58b0671da9c01d12be13fc2144
Tony <tomcatbuzz@yahoo.com>
2018-09-08 00:42:26 -0400

    added auth

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,96 +1,43 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
+const multer = require('multer');
+const checkAuth = require('../auth/check-auth');
 
-const Product = require('../models/products');
+const ProductsController = require('../controllers/products');
 
-router.get('/', (req, res, next) => {
-  Product.find()
-  .select('name price _id')
-  .exec()
-  .then(docs => {
-    const response = {
-      count: docs.length,
-      products: docs.map(doc => {
-        return {
-          name: doc.price,
-          price: doc.price,
-          _id: doc._id,
-          request: {
-
-          }
-        }
-      })
-    };
-    res.status(200).json(response);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({error: err});
-  });
-});
-
-router.post('/', (req, res, next) => {
-  const product = new Product({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    price: req.body.price
-  });
-  product.save().then(result => {
-    console.log(result);
-    res.status(201).json({
-      message: 'Handling POST requests to /products',
-      createdProduct: result
-    });
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({error: err});
-  });
-});
-
-router.get('/:productId', (req, res, next) => {
-  const id = req.params.productId;
-  Product.findById(id)
-  .exec()
-  .then(doc => {
-    console.log('From Database', doc);
-    res.status(200).json(doc);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({error: err});
-  });
-});
-
-router.patch('/:productId', (req, res, next) => {
-  const id = req.params.productId;
-  const updateOps = {};
-  for (const ops of req.body) {
-    updateOps[ops.propName] = ops.value;
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
   }
-  Product.update({ _id: id }, { $set: updateOps })
-  .exec()
-  .then(result => {
-    res.status(200).json(result);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({error: err});
-  });
 });
 
-router.delete('/:productId', (req, res, next) => {
-  const id = req.params.productId;
-  Product.findByIdAndRemove(id)
-  .exec()
-  .then(result => {
-    res.status(200).json(result);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({error: err});
-  });
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage, 
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
 });
+
+router.get('/', ProductsController.products_get_all);
+
+router.post('/', checkAuth, upload.single('productImage'), ProductsController.products_create_product);
+
+router.get('/:productId', ProductsController.products_get_product);
+
+router.patch('/:productId', checkAuth, ProductsController.products_update_product);
+
+router.delete('/:productId', checkAuth, ProductsController.products_delete_product);
 
 module.exports = router;
--- app.js
+++ app.js
@@ -3,17 +3,28 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
+
+require('dotenv').config()
 
 mongoose.connect(
-  'mongodb+srv://tomcatbuzz:' +      process.env.MONGO_ATLAS_PW + '@cluster0-gu6vt.mongodb.net/test?retryWrites=true', 
+  'mongodb+srv://tomcatbuzz:' + process.env.MONGO_ATLAS_PW + '@cluster0-gu6vt.mongodb.net/node-rest?retryWrites=true', 
   { 
     useNewUrlParser: true 
+  })
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed');
   }); 
-
+ 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -29,6 +40,7 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error('Not found');
